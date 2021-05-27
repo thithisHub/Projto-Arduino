81bfc36ae54396fb9f87735a86cb336b2b33a313
thithisHub <60509523+thithisHub@users.noreply.github.com>
2021-05-26 23:27:51 -0300

    Update mqtt_teste.js
    documentaçao do codigo node

--- mqtt_teste.js
+++ mqtt_teste.js
@@ -1,16 +1,12 @@
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://test.mosquitto.org')
+var mqtt = require('mqtt')// modulo npm para uso do protocolo mqqt
+var client  = mqtt.connect('mqtt://test.mosquitto.org')// estabelece comunicaçao com broker especificado
  
-client.on('connect', function () {
+client.on('connect', function () {//
     console.log("conectou");
-  client.subscribe('enviar_msgs_mqtt_arduino_regador', function (err) {
-    if (!err) {
-      client.publish('receber_msgs_mqtt_arduino_regador', 'Hello mqtt')
-    }
+  client.subscribe('enviar_msgs_mqtt_arduino_regador', function (err) {// ao se conectar subscreve o canal de comunicaçao especificado   
   })
 })
  
 client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
+  console.log(message.toString())// recebe e printa informaçoes enviadas pelo arduino
 })
